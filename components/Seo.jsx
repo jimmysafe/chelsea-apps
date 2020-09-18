34b19cb94c18807ce4847718633a6e0f60b6a0f1
Gianmarco Ciaffardini <marco@chelsea-apps.com>
2020-09-18 11:40:04 +0200

    add Seo to layout component

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -2,13 +2,15 @@
 import Head from 'next/head'
 
 const Seo = ({ page }) => {
-    const { meta_title, meta_description } = page.data
-    return (
-        <Head>
-            <title>{meta_title && meta_title[0] ? meta_title[0].text : 'Chelsea Apps'} | Chelsea Apps</title>
-            <meta name="description" content={meta_description && meta_description[0] ? meta_description[0].text : 'Chelsea Apps'}/>
-        </Head>
-    )
+    if(page){
+        const { meta_title, meta_description } = page.data
+        return (
+            <Head>
+                <title>{meta_title && meta_title[0] ? meta_title[0].text : 'Chelsea Apps'} | Chelsea Apps</title>
+                <meta name="description" content={meta_description && meta_description[0] ? meta_description[0].text : 'Chelsea Apps'}/>
+            </Head>
+        )
+    } else return null
 }
 
 export default Seo
