71109bc482ce2d935cde981de025b70fc7523ff0
Gianmarco Ciaffardini <marco@chelsea-apps.com>
2020-09-23 15:38:31 +0200

    add preview

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,10 @@
 import Seo from './Seo'
 import Footer from './Footer'
 import Nav from './Nav'
+import { useRouter } from 'next/router'
 
 const Layout = (props) => {
-
+    const router = useRouter()
     const textColor = props.page ? props.page.data.page_text_color : '#1E1E1E'
     const bgColor = props.page ? props.page.data.page_color : '#FEE315'
 
@@ -11,6 +12,14 @@ const Layout = (props) => {
         <>
         <Seo page={props.page}/>
         <Nav textColor={textColor} bgColor={bgColor}/>
+        {props.preview && 
+            <div className="preview">
+                <p>Preview Mode</p>
+                <div className="exit-preview" onClick={() => router.push('/api/exit-preview')}>
+                    <p>Exit</p>
+                </div>
+            </div>
+        }
         <section id="content" role="main" className="content-wrapper">
             <div className="wrapper">
                 {props.children}
